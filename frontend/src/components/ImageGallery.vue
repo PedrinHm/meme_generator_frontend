8358cfb186877488889a75be9115e800493e9508
Pedro Mendes <pedrohm441@gmail.com>
2024-05-26 19:44:57 -0300

    Versão 2

--- frontend/src/components/ImageGallery.vue
+++ frontend/src/components/ImageGallery.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="image-gallery">
-    <header ref="header" class="gallery-header">Memes da Galera</header>
+    <header ref="header" class="gallery-header">
+      <img src="@/assets/logo1.png" alt="Logo 1" class="logo">
+      <div class="header-title">Memes da Galera</div>
+      <img src="@/assets/logo2.png" alt="Logo 2" class="logo">
+    </header>
     <div ref="imageContainer" class="image-container">
       <img :src="currentImageUrl" alt="Loaded image" v-if="currentImageUrl">
     </div>
@@ -93,8 +97,8 @@ html, body {
   height: 100%;
   overflow: hidden;
   font-family: 'Rubik', sans-serif;
-  background-color: #343541; 
-  color: white; 
+  background-color: #343541; /* Cinza escuro */
+  color: white; /* Texto branco */
 }
 
 .image-gallery {
@@ -102,17 +106,41 @@ html, body {
   flex-direction: column;
   height: 100vh;
   overflow: hidden;
-  background-color: #343541;
+  background-color: #343541; /* Fundo cinza escuro */
 }
 
 .gallery-header {
-  background-color: #202123; 
+  background-color: #202123; /* Cinza escuro */
   color: white;
   text-align: center;
   font-size: 2rem;
   padding: 10px 0;
   box-sizing: border-box;
-  font-family: 'Rubik', sans-serif; 
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative; /* Permitir posicionamento absoluto das logos */
+}
+
+.header-title {
+  flex-grow: 1;
+  text-align: center;
+}
+
+.logo {
+  height: 50px; /* Ajuste conforme necessário */
+  border: none !important; /* Remover bordas das logos */
+  margin: 0 10px; /* Adicionar espaço ao redor das logos */
+  position: absolute;
+}
+
+.logo:first-child {
+  left: 10px; /* Ajuste conforme necessário */
+}
+
+.logo:last-child {
+  right: 10px; /* Ajuste conforme necessário */
 }
 
 .image-container {
@@ -121,13 +149,13 @@ html, body {
   justify-content: center;
   align-items: center;
   overflow: hidden;
-  background-color: #343541; 
+  background-color: #343541; /* Fundo cinza escuro */
 }
 
-.image-gallery img {
+.image-container img {
   max-width: 100%;
   max-height: 100%;
   object-fit: contain;
-  border: 5px solid #343541; 
+  border: 5px solid #343541; /* Bordas cinza escuro */
 }
 </style>
--- frontend/src/components/ImageUploadForm.vue
+++ frontend/src/components/ImageUploadForm.vue
@@ -1,17 +1,30 @@
 <template>
   <div class="upload-form">
-    <header class="form-header">Envie sua Imagem para Gerar um Meme</header>
+    <header class="form-header">
+      <div class="header-title">Envie sua Imagem para Gerar um Meme</div>
+    </header>
     <form @submit.prevent="uploadImage">
-      <input type="file" @change="onFileChange" />
+      <div v-if="selectedFile || uploadedImageUrl" class="preview">
+        <p v-if="selectedFile && !uploadedImageUrl">Imagem selecionada:</p>
+        <p v-if="uploadedImageUrl">Meme gerado com sucesso!</p>
+        <img :src="uploadedImageUrl ? uploadedImageUrl : imagePreview" :alt="uploadedImageUrl ? 'Generated Meme' : 'Preview'" />
+      </div>
+      <label class="file-input-label">
+        Anexar Imagem <i class="fas fa-paperclip"></i>
+        <input type="file" @change="onFileChange" />
+      </label>
       <button type="submit" :disabled="!selectedFile || isLoading">
         <span v-if="isLoading">Carregando...</span>
         <span v-else>Enviar</span>
       </button>
     </form>
-    <div v-if="uploadedImageUrl" class="result">
-      <p>Meme gerado com sucesso!</p>
-      <img :src="uploadedImageUrl" alt="Generated Meme" />
-    </div>
+    <footer class="site-footer">
+      <p>© 2024 Pedro Mendes. Todos os direitos reservados.</p>
+      <p>
+        <a href="https://www.linkedin.com/in/pedro-mendes-919b51181/">Linkedin</a> |
+        <a href="https://github.com/PedrinHm">GitHub</a>
+      </p>
+    </footer>
   </div>
 </template>
 
@@ -25,16 +38,25 @@ export default {
       selectedFile: null,
       uploadedImageUrl: null,
       isLoading: false,
+      imagePreview: null,
     };
   },
   methods: {
     onFileChange(event) {
       this.selectedFile = event.target.files[0];
+      this.uploadedImageUrl = null; // Reseta a URL do meme gerado quando uma nova imagem é selecionada
+      if (this.selectedFile) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          this.imagePreview = e.target.result;
+        };
+        reader.readAsDataURL(this.selectedFile);
+      }
     },
     async uploadImage() {
       if (!this.selectedFile) return;
 
-      this.isLoading = true; 
+      this.isLoading = true; // Inicia o estado de carregamento
 
       const formData = new FormData();
       formData.append('file', this.selectedFile);
@@ -49,7 +71,7 @@ export default {
       } catch (error) {
         console.error('Erro ao enviar a imagem:', error);
       } finally {
-        this.isLoading = false; 
+        this.isLoading = false; // Encerra o estado de carregamento
       }
     },
   },
@@ -63,23 +85,23 @@ html, body {
   height: 100%;
   overflow: hidden;
   font-family: 'Rubik', sans-serif;
-  background-color: #343541;
-  color: white; 
+  background-color: #343541; /* Cinza escuro */
+  color: white; /* Texto branco */
 }
 
 .upload-form {
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 0; 
+  margin-top: 0; /* Remover margem superior */
   padding: 0;
-  background-color: #343541;
-  color: white; 
-  min-height: 100vh; 
+  background-color: #343541; /* Fundo cinza escuro */
+  color: white; /* Texto branco */
+  min-height: 100vh; /* Garantir que o fundo preencha toda a altura da tela */
 }
 
 .form-header {
-  background-color: #202123; 
+  background-color: #202123; /* Cinza escuro */
   color: white;
   text-align: center;
   font-size: 1.5rem;
@@ -87,6 +109,15 @@ html, body {
   box-sizing: border-box;
   width: 100%;
   margin-bottom: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative; /* Permitir posicionamento absoluto das logos */
+}
+
+.header-title {
+  flex-grow: 1;
+  text-align: center;
 }
 
 form {
@@ -95,8 +126,18 @@ form {
   align-items: center;
 }
 
-input[type="file"] {
+.file-input-label {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  font-size: 1.5rem;
+  color: #ffffff;
   margin-bottom: 10px;
+  font-family: 'Rubik', sans-serif; /* Aplicando a mesma fonte */
+}
+
+.file-input-label input {
+  display: none;
 }
 
 button {
@@ -105,6 +146,8 @@ button {
   border: none;
   padding: 10px 20px;
   cursor: pointer;
+  border-radius: 5px; /* Bordas arredondadas */
+  font-family: 'Rubik', sans-serif; /* Aplicando a mesma fonte */
 }
 
 button:disabled {
@@ -112,15 +155,39 @@ button:disabled {
   cursor: not-allowed;
 }
 
-.result {
+.preview, .result {
   margin-top: 20px;
   text-align: center;
 }
 
-.result img {
+.preview img, .result img {
   max-width: 100%;
   max-height: 300px;
   object-fit: contain;
-  border: 5px solid #343541; 
+  border: 5px solid #343541; /* Bordas cinza escuro */
+}
+
+.site-footer {
+  background-color: #202123; /* Cinza escuro */
+  color: #ccc; /* Cor do texto do rodapé */
+  text-align: center;
+  padding: 20px;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+
+.site-footer p {
+  margin: 5px 0;
+}
+
+.site-footer a {
+  color: #8cae77; /* Cor dos links do rodapé */
+  text-decoration: none;
+  margin: 0 10px;
+}
+
+.site-footer a:hover {
+  text-decoration: underline;
 }
 </style>
